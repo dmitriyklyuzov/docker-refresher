2a26ac65837f7a73d31ea9d60b1f9e6f18f7041a
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2024-08-10 08:30:53 -0400

    Modified the Node app to connect to Mongo and list databases

--- app/server.js
+++ app/server.js
@@ -1,12 +1,39 @@
 const { createServer } = require('node:http');
+const MongoClient = require('mongodb').MongoClient;
 
+// Server params
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
+// Mongo params
+const mongoUri = "mongodb://mongoadmin:password@localhost:27017";
+const databaseName = "my-db";
+
+const server = createServer(async (req, res) => {
+  
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! The server is running.');
+  
+  try {
+    // Connect to the MongoDB client
+    const client = await MongoClient.connect(mongoUri);
+
+    // List databases
+    const databaseList = await client.db().admin().listDatabases();
+    const dbList = databaseList.databases.map(db => db.name).join(', ');
+
+    // close the MongoDB connection
+    client.close();
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello World! The server is running.\n\nAvailable databases: ${dbList}`);
+  } catch (err) {
+    // handle errors
+    console.log("Error connecting to MongoDB: ", err)
+    res.statusCode = 500;
+    
+    res.end('Internal Server Error');
+  }
 });
 
 server.listen(port, hostname, () => {
